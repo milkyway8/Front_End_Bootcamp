da56b421a7d5bf313bef9f8d8dab0a261e783d7a
Johnathan <josornov5@gmail.com>
2017-07-09 11:39:08 -0500

    Javascripting

---
+++ workshop/javascripting/array-filtering.js
@@ -0,0 +1,15 @@
+var umbers = [1,2,3,4,5,6,7,8,9,10];
+var filtered = numbers.filter(function evenNumbers (number){
+    return number % 2 === 0;
+});
+console.log(filtered);
+
+/*
+numbers = [1,2,3,4,5,6,7,8,9,10];
+filtered = numbers.filter(function(number){
+    return number % 2 === 0;
+});
+console.log(filtered);
+*/
+
+/*function anonima
---
+++ workshop/javascripting/functions.js
@@ -0,0 +1,5 @@
+function eat(food) {
+    return food + ' tasted really good.';
+}
+/*What if the argument is not a string*/
+console.log(eat('bananas'));
